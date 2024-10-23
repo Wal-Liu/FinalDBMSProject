bae25bba9bb39c3c7dfeed279d5a2b47fd9b0e07
hvmegy <hamegy08lleo@gmail.com>
2024-10-23 21:39:46 +0700

    init class

---
+++ WpfApp1/Object/Kho.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Object
+{
+    internal class Kho
+    {
+        private int maKho;
+        private string tenKho;
+        private string diaChi;
+        private string SDT;
+        Kho(int maKho, string tenKho, string diaChi, string SDT)
+        {
+            this.maKho = maKho;
+            this.tenKho = tenKho;  
+            this.diaChi = diaChi;
+            this.SDT = SDT;
+        }
+    }
+}
