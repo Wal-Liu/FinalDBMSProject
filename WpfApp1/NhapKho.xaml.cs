1725ac145b60ed995dc04250185d6dd9ee03d88e
Wall_Liu <tuongthanhien@gmail.com>
2024-10-24 09:19:07 +0700

    lay het san pham tu database va load vao combobox

--- WpfApp1/NhapKho.xaml.cs
+++ WpfApp1/NhapKho.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,14 +21,67 @@ namespace WpfApp1
         /// </summary>
         public partial class NhapKho : Window
         {
+                string strCon = @"Data Source=WALL-LIU;Initial Catalog=QLSanPham;Integrated Security=True;Encrypt=false";
+                SqlConnection sqlcon = null;          
                 public NhapKho()
                 {
                         InitializeComponent();
+                        btnMo_Click();
                 }
 
                 private void btnNhapHang_Click(object sender, RoutedEventArgs e)
                 {
                         MessageBox.Show("Nhap Hang thanh cong");
+                }
+
+                private void loadSanPham()
+                {
+                        if (sqlcon != null && sqlcon.State == ConnectionState.Open)
+                        {
+                                using (SqlConnection connection = new SqlConnection(strCon))
+                                {
+                                        connection.Open();
+                                        using (SqlCommand command = new SqlCommand("proc_LayHetSanPham", connection))
+                                        {
+                                                command.CommandType = CommandType.StoredProcedure;
+                                                SqlDataReader reader = command.ExecuteReader();
+                                                while (reader.Read())
+                                                {
+                                                        ComboBoxItem comboBoxItem = new ComboBoxItem();
+                                                        comboBoxItem.Content = reader["tenSP"].ToString();
+                                                        comboBoxItem.Tag = reader["maSP"].ToString();
+                                                        cbbSanPham.Items.Add(comboBoxItem);
+
+                                                        MessageBox.Show(comboBoxItem.Tag.ToString());
+
+                                                }
+                                        }
+                                }
+                        }
+                }
+
+
+                private void btnMo_Click()
+                {
+                        try
+                        {
+                                if (sqlcon == null)
+                                {
+                                        sqlcon = new SqlConnection(strCon);
+                                }
+                                sqlcon = new SqlConnection(strCon);
+                                if (sqlcon.State == ConnectionState.Closed)
+                                {
+                                        sqlcon.Open();
+                                        MessageBox.Show("Ket noi thanh cong");
+                                        loadSanPham();
+                                }
+                        }
+                        catch (Exception ex)
+                        {
+                                MessageBox.Show(ex.Message);
+
+                        }
+                }
         }
-    }
 }
